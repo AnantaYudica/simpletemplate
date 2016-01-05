8f61bcee202c766fe403dfdf3a0e946c81e35c78
AnantaYudica <nta040689@gmail.com>
2016-01-05 10:37:47 +0700

    move to Properties.h to folder typehelper

---
+++ master/typehelper/Properties.h
@@ -0,0 +1,25 @@
+/*
+ * Properties.h
+ *
+ *  Created on: 5 Jan 2016
+ *      Author: "Ananta Yudica"
+ */
+
+#ifndef MASTER_TYPEHELPER_PROPERTIES_H_
+#define MASTER_TYPEHELPER_PROPERTIES_H_
+
+
+namespace typehelper{
+
+struct Property{
+	struct Parameter {
+		static const size_t SIZE_FIRST_LEVEL = 300;
+		static const size_t SIZE_SECOND_LEVEL = 200;
+		static const size_t SIZE_THIRD_LEVEL = 100;
+	};
+};
+
+};
+
+
+#endif /* MASTER_TYPEHELPER_PROPERTIES_H_ */
